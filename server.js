263c84dc32644838e417acd12fb70f2cdb3e44cd
Robby Brosman <robbybro@extrahop.com>
2016-03-07 23:32:17 -0800

    WIP
    * start moving towards more generic mashup
    * create scaffolding for simple UI to mashup accounts

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 // uname: trumpsand
 // password: *****************
+'use strict';
 
+var express     = require('express');       // call express
+var bodyParser  = require('body-parser'); // parses body data from http POST
+var Twit        = require('twit');
+var ent         = require('ent');
+var request     = require('request');   // helps make http calls
 
-var Twit = require('twit');
-var ent = require('ent');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// We set up a web server on port 3000 using express.js because we're hosting on Nodejitsu. If you're
-// just running this on a server, or perhaps some non-Nodejitsu service, you can omit
-// all the code from here up to "-- End express"
-var app = require('express').createServer();
+// app.get('/', function (req, res) {
+//   res.send('Hi.');
+// });
+
 app.get('/', function (req, res) {
-  res.send('Hi.');
+  makeSwag(res);
 });
-app.listen(3000);
 // -- End express
+
+
 
 // Our twitter developer API info is in a file called 'config.js'
 var T = new Twit(require('./config.js'));
 
-function makeSwag() {
-
+function makeSwag(res) {
   // set up our two data sources (you can paste these in your browser to see the results, in JSON format)
   // URI for all recent #trump tweets with 'and' in them
   var trump = {q: "from:realDonaldTrump", count: 100, result_type: "recent"}; 
@@ -27,7 +34,6 @@ function makeSwag() {
   var bernie = {q: "from:BernieSanders", count: 100, result_type: "recent"};
   var trumps = [];
   var bernies = [];
-  var berniesShort = [];
   var pre = "";
   var post = "";
   var tweet = "";
@@ -42,39 +48,45 @@ function makeSwag() {
     // more than 20 characters into the tweet
     for (var i = 0; i < results.length; i++) {
       var text = results[i].text;
-      if (text.indexOf(' and ') !== -1 && text.indexOf('RT') == -1 && text.indexOf(' and ') > 20) {
+      if (
+        text.indexOf(' and ') !== -1 &&
+        text.indexOf('RT') == -1 &&
+        text.indexOf(' and ') > 20
+      ) {
         trumps.push(text);
       }
     }
-    console.log(trumps.length);
+    console.log('# trumps: ' + trumps.length);
     // get the bernie tweets
     T.get('search/tweets', bernie, function(err, data) {
       if(err) throw err;
       var results = data.statuses;
       // for each bernie tweet, find the ones with ' and ' or a comma, and then only if the and or the comma
       // appear somewhat in the middle (30-90 characters). push those to the bernies array.
-      // OR if the whole bernie tweet is less than 50 chars, we can prepend 'and' to it and push it to berniesShort
       for (var i = 0; i < results.length; i++) {
         var text = results[i].text;
-        //console.log(text);
 
-        if (text.indexOf(' and ') !== -1 || text.indexOf(', ') !== -1) {
-          if ((text.indexOf(' and ') < 90 && text.indexOf(' and ') > 30) || (text.indexOf(', ') < 90 && text.indexOf(', ') > 30)) {
+        if (
+          text.indexOf(' and ') !== -1 ||
+          text.indexOf(', ') !== -1
+        ) {
+          if (
+            (text.indexOf(' and ') < 90 && text.indexOf(' and ') > 30) ||
+            (text.indexOf(', ') < 90 && text.indexOf(', ') > 30)
+          ) {
             bernies.push(text);
           }
         }
-        else if (text.length < 50) {
-          berniesShort.push(' and ' + text.toLowerCase());
-        }
       }
+      console.log('#bernies: ' + bernies.length);
 
       // OKAY now we have 'trumps', an array containing a subset of trump tweets with 'and' in them,
-      // and 'bernies', an array containing a subset of @berniebot tweets with 'and' or ','
+      // and 'bernies', an array containing a subset of bernie tweets with 'and' or ','
 
       console.log(bernies.length);
 
 
-      // now we randomize whether tweet takes the form "@berniebot and #trump" or "#trump and @berniebot"
+      // now we randomize whether tweet takes the form bernie-and-trump  or trump-and-bernie
       var berniefirst = true;
       var text = "";
       if (Math.random() < 0.5) {
@@ -104,18 +116,18 @@ function makeSwag() {
       }
 
       // now we pick a random bernie or a random trump for end of our tweet
-
       if (berniefirst) {
         text = trumps[Math.floor(Math.random() * trumps.length)];
       }
       else {
-        // this is just adding on those < 50 char bernie quips since it only matters in the postfix context, not the prefix
-        bernies = bernies.concat(berniesShort);
         text = bernies[Math.floor(Math.random() * bernies.length)];
       }
 
       // this time we extract the second half of the source tweet and put it in 'post'
-      if (text.indexOf(' and ') !== -1) {
+      if (!text) {
+        post = ' and milk and cookies';
+      }
+      else if (text.indexOf(' and ') !== -1) {
         post = text.substr(text.indexOf(' and ') + 5, 140);
       }
       else if (text.indexOf(', ') !== -1) {
@@ -131,29 +143,28 @@ function makeSwag() {
       tweet = ent.decode(tweet);
       // truncate to 140 chars so twitter doesn't reject it (most tweets are < 140 but some aren't)
       tweet = tweet.substr(0, 140);
-      console.log(tweet);
-      console.log(tweet.length);
 
-      // tweet it!	
+      // TODO: don't tweet for now, but sometday
+      // T.post('statuses/update', {
+      //   status: tweet
+      // }, function (err, reply) {});
 
-      T.post('statuses/update', {
-        status: tweet
-      }, function (err, reply) {});
-
-
+      res.send(tweet);
     });
   });
 }
+app.use(express.static('/'));
+app.listen(3000);
+console.log('twittermash is running');
 
 // Tweet once when we start up the bot
-makeSwag();
 
 // Tweet every 15 minutes
-setInterval(function () {
-  try {
-    makeSwag();
-  }
-  catch (e) {
-    console.log(e);
-  }
-}, 1000 * 60 * 15);
+// setInterval(function () {
+//   try {
+//     makeSwag();
+//   }
+//   catch (e) {
+//     console.log(e);
+//   }
+// }, 1000 * 60 * 15);
